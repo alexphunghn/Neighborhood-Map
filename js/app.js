dd6b595be52a4e82e998a09ebc2070b482309ad8
alexphunghn <Alex@Alexs-MacBook-Pro.local>
2017-01-26 23:01:49 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var places = [
+	{
+		name: 'The Raines Law Room',
+		address: '48 W 17th St, New York, NY 10011',
+		lat: 40.738782,
+		lng: -73.994566,
+		info: 'Speakeasy Pouring Prohibition-Era Drinks',
+		tags: ['drinking', 'nightlife', 'bars'],
+	},
+	{
+		name: 'Apple Store',
+		address: '767 5th Ave, New York, NY 10153',
+		lat: 40.763738,
+		lng: -73.972299,
+		info: 'Retailer of Apple Products and Accessories',
+		tags: ['shopping', 'technology']
+	},
+	{
+		name: 'Big Gay Ice Cream Shop',
+		address: '125 E 7th St, New York, NY 10009',
+		lat: 40.726434,
+		lng: -73.984075,
+		info: 'Trendy Soft-Serve with Creative Flavors',
+		tags: ['food']
+	},
+	{
+		name: 'Dylan\'s Candy Bar',
+		address: '1011 3rd Ave, New York, NY 10065',
+		lat: 40.762234,
+		lng: -73.965738,
+		info: 'Two Floors of Sweet Treats',
+		tags: ['kids', 'shopping', 'food']
+	},
+	{
+		name: 'Serendipity 3',
+		address: '225 E 60th St, New York, NY 10022',
+		lat: 40.761887,
+		lng:  -73.964858,
+		info: 'Legendary Dessert Destination',
+		tags: ['food']
+	},
